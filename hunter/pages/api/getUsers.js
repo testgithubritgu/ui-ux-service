c0a134091c1c862799599cc25ce4102f0d7d13ec
testgithubritgu <swapnilraut1698@gmail.com>
2026-04-11 18:35:59 +0530

    test

--- hunter/pages/api/getUsers.js
+++ hunter/pages/api/getUsers.js
@@ -14,6 +14,7 @@ async function handler(req, res) {
             res.status(500).json({
                 success: false,
                 error: "Internal Server Error"
+                
             });
         }
     }else if (req.method === "POST") {
--- hunter/pages/api/sign-up.js
+++ hunter/pages/api/sign-up.js
@@ -1,16 +1,24 @@
 import User from "../../model/userSchema"
-
-export default async function handler(req, res) {
+import bcrypt from "bcrypt"
+import connectDb from "../../middleware/mongoose"
+ async function handler(req, res) {
     try {
         const { email, name, password } = req.body
         const hasUser = await User.findOne({ email })
         if (hasUser) return res.status(404).json({ message: "user already exist !" })
-        const newUser = await User.create({
-            email, name, password
-        })
 
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        
+        const newUser = await User.create({
+            email, name, password: hashedPassword
+        })
+console.log(newUser)
         res.status(201).json({ message: "user created succesfully", newUser })
     } catch (error) {
-        res.status(500).json({message:"internal server error"})
+        res.status(500).json({ message: "internal server error" })
     }
 }
+
+
+export default connectDb(handler)
